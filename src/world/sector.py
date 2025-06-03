72c32fddddf3d9a80fda769945b1c8e67f43ff61
jiwon-hae <hjiwon1994@gmail.com>
2025-06-03 13:24:09 -0700

    chore: Move sector and sequences to world

---
+++ src/world/sector.py
@@ -0,0 +1,24 @@
+from collections import defaultdict
+from rendering.gaussian_splatting import GaussianSplatting
+from spaces.elevation_map import ElevationMap
+from spaces.occupancy_map import OccupancyMap
+from spaces.boundary_polygon import BoundaryPolygon
+
+class Sector:
+    def __init__(self, seq_id, sector_id):
+        self.seq_id = seq_id
+        self.sector_id = sector_id
+        self.elevation_map = ElevationMap(seq_id, sector_id)
+        self.occupancy_map = OccupancyMap(seq_id, sector_id)
+        self.boundary = BoundaryPolygon(seq_id, sector_id)
+        self.gaussian_model = GaussianSplatting(seq_id, sector_id)
+
+        self.prev = None
+        self.next = None
+
+    def __repr__(self):
+        return f"Sector {self.sector_id}"
+        
+        
+    
+    
