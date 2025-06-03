c2f8b5140ad641f99124b78cd2b5af066352bde5
jiwon-hae <hjiwon1994@gmail.com>
2025-06-03 13:42:00 -0700

    feat: Add model path to the gaussian splatting

--- src/rendering/gaussian_splatting.py
+++ src/rendering/gaussian_splatting.py
@@ -6,7 +6,7 @@ import json
 class GaussianSplatting:
     def __init__(self, seq_id, sector_id):
         self.db = RocksDB()
-        self.gs = self.get_gaussian_splatting(seq_id, sector_id, sector_id)
+        self.model_path = self.get_gaussian_splatting(seq_id, sector_id, sector_id)
     
     def get_gaussian_splatting(self, seq_id, sector_id):
         date, session = seq_id.split('/')
@@ -18,16 +18,9 @@ class GaussianSplatting:
             }, sort_keys=True)
         data  = self.db.get(key)
         file_name = f'{date}_{session}_{sector_id}.ply'
+        
         with open(file_name, "wb") as f:
             f.write(data)
-
-        try:
-            pcd = o3d.io.read_point_cloud(file_name)
-            if len(pcd.points) == 0:
-                    print("❌ Loaded PLY file, but contains no points.")
-            else:
-                print(f"✅ Loaded PLY file with {len(pcd.points)} points.")
-            return pcd
-        except Exception as e:
-            print("❌ Failed to load Gaussian splat:", e)
+        
+        return file_name
         
