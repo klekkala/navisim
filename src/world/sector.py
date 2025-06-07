a095b6bb895abd4f28c8cb6397df2bd0b3a3cd8c
jiwon-hae <hjiwon1994@gmail.com>
2025-06-06 18:16:24 -0700

    chore: Update sector id format

--- src/world/sector.py
+++ src/world/sector.py
@@ -7,11 +7,11 @@ from spaces.boundary_polygon import BoundaryPolygon
 class Sector:
     def __init__(self, seq_id, sector_id):
         self.seq_id = seq_id
-        self.sector_id = sector_id
-        self.elevation_map = ElevationMap(seq_id, sector_id)
-        self.occupancy_map = OccupancyMap(seq_id, sector_id)
-        self.boundary = BoundaryPolygon(seq_id, sector_id)
-        self.gaussian_model = GaussianSplatting(seq_id, sector_id)
+        self.sector_id = f'sector{sector_id}'
+        self.elevation_map = ElevationMap(seq_id, self.sector_id)
+        self.occupancy_map = OccupancyMap(seq_id, self.sector_id)
+        self.boundary = BoundaryPolygon(seq_id, self.sector_id)
+        self.gaussian_model = GaussianSplatting(seq_id, self.sector_id)
 
         self.prev = None
         self.next = None
