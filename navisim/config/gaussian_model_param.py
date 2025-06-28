163bf8c2fb6b981220e6149d59649b1551be5050
jiwon-hae <hjiwon1994@gmail.com>
2025-06-27 19:16:22 -0700

    chore: Update submodule imports

--- navisim/config/gaussian_model_param.py
+++ navisim/config/gaussian_model_param.py
@@ -1,12 +1,8 @@
 import os
 import sys
 
-# Get the absolute path to your project root
-# Assuming your structure is: project_root/experimental/test.ipynb
-project_root = os.path.abspath(os.path.join(os.getcwd(), '..', '..'))
-
-# Add the third_party/gs directory to Python path
-third_party_path = os.path.join(project_root, 'third_party', 'gaussian-splatting')
+project_root = os.path.abspath(os.path.join(os.getcwd(), '..'))
+third_party_path = os.path.join(project_root, 'third_party', 'gaussian_splatting')
 if third_party_path not in sys.path:
     sys.path.insert(0, third_party_path)
 
